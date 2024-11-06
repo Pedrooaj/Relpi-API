9e83cf1ea820ab984d75fff33594b49777081bea
Pedrooaj <pedroantonio5735@gmail.com>
2024-11-06 12:50:34 -0300

    update

--- src/routes/Index.ts
+++ src/routes/Index.ts
@@ -5,16 +5,7 @@ import multer from "multer";
 
 const index: Router = Router();
 
-
-
-
-
-index.get("/", (req: Request, res: Response) => {
-    res.json({
-        index: "Hello"
-    })
-});
-
+// rota para enviar audio
 index.post("/audio", upload.single('audio'),(req: Request, res: Response): void => {
     if(!req.file){
         res.status(400).json({
